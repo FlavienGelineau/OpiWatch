67f3a9e3af04375685a0d4d9dee5a7fbc7e3fb14
Flavien Gelineau <you@example.com>
2019-02-20 16:25:39 +0100

    python files added

---
+++ explo_bidmc.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+'''
+RESP : respiraiton
+pleth : plethysmograph. pulse oximeter pleth
+V : tension ecg
+average : moyenne glissante.
+II : inconnu au bataillon
+
+'''
+def get_patient_bidmc_csv_n(n):
+    id = str(n) if n>=10 else '0'+str(n)
+    breath = pd.read_csv('../data/bidmc_csv/bidmc_{}_Breaths.csv'.format(id))
+    numerics = pd.read_csv('../data/bidmc_csv/bidmc_{}_Numerics.csv'.format(id))
+    signals = pd.read_csv('../data/bidmc_csv/bidmc_{}_Signals.csv'.format(id))
+
+    age = pd.read_csv('../data/bidmc_csv/bidmc_{}_Fix.txt'.format(id),
+                      error_bad_lines=False,
+                      warn_bad_lines=False,
+                      sep = ':').to_dict()
+    return breath, numerics, signals, age
+
+def feature_extraction(signals, age_dict):
+    try:
+        age = [int(vals['Age']) for _, vals in age_dict.items()][0]
+    except:
+        print('error with age')
+        age = None
+    res = {'age':age,
+            'mean resp':signals[' RESP'].mean(),
+            'std resp':signals[' RESP'].std(),
+            'mean pleth':signals[' PLETH'].mean(),
+            'std pleth':signals[' PLETH'].std(),
+            'mean II':signals[' II'].mean(),
+            'std II':signals[' II'].std(),
+            }
+    try:
+        res['mean V'] =signals[' V'].mean()
+        res['std V']= signals[' V'].std()
+        res['mean AVR']= signals[' AVR'].mean()
+        res['std AVR'] = signals[' AVR'].std()
+    except:
+        res['mean V'] = None
+        res['std V'] = None
+        res['mean AVR'] = None
+        res['std AVR'] = None
+    return res
+
+if __name__ =='__main__':
+    health_signals = []
+    for n in range(1,54):
+        print(n)
+        breath, numerics, signals, age = get_patient_bidmc_csv_n(n)
+        health_signals.append(feature_extraction(signals, age))
+    df = pd.DataFrame(health_signals)
+    print(df.corr())
+    print(df.mean())
---
+++ explo_ptdb.py
@@ -0,0 +1,30 @@
+import os
+
+from tqdm import tqdm
+from wfdb import io, plot
+import pandas as pd
+
+# The folder where you want to store your data
+data_folder = '../data/'
+# First get the list of available records and then download
+# those records and store them in data_folder.
+record_names = io.get_record_list('ptbdb')
+#io.dl_database('ptbdb', data_folder, record_names)
+
+# Read the first record
+record_name = record_names[0]
+print(record_name)
+record = io.rdrecord(record_name="../data/ptdb/"+record_name)
+
+records = []
+for record_name in tqdm(record_names):
+    record = io.rdrecord(record_name=os.path.join('../data/ptdb/', record_name))
+    # label = comments_to_dict(record.comments)['Reason for admission'][1:]
+    label = record.comments[4].split(": ")[-1]
+    patient = record_name.split('/')[0]
+    signal_length = record.sig_len
+    records.append({'name': record_name, 'label': label, 'patient': patient, 'signal_length': signal_length})
+
+channels = record.sig_name
+df_records = pd.DataFrame(records)
+print(df_records)
