f9493b4ccd72c31c4928e037519e5ae419e13339
Maxence Lecanu <maxence.lecanu@ensta-paristech.fr>
2019-03-16 15:02:28 +0100

    moved asclepios2 to be main version of asclepios

--- backend/asclepios/lib/colors.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = {
-    bold : chalk.bold,
-    connected : chalk.rgb(0, 150, 0),
-    error : chalk.rgb(200, 0, 0),
-    disconnected : chalk.rgb(200, 200, 0),
-    termination : chalk.rgb(255, 255, 255),
-}
